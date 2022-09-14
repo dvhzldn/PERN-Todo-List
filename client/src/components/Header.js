31e776d3f9d65b161808b21c0e51aa4295b307eb
Dave Hazeldean <103963321+dvhzldn@users.noreply.github.com>
2022-09-14 01:29:41 +0100

    Code rewritten from scratch using React. Rest API CRUD routes written and operational. Todo: use database to store the data of the todos

---
+++ client/src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Header() {
+	return (
+		<div>
+			<nav className="nav">
+				<div className="nav-left"></div>
+				<div className="nav-right"></div>
+			</nav>
+		</div>
+	);
+}
+
+export default Header;
